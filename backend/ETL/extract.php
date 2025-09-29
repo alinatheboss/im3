ca230056766ca00f8823f9cb937f6a631ebd55aa
alinatheboss <alina.bosshard@stud.fhgr.ch>
2025-09-29 14:58:41 +0200

    ETL Test

---
+++ backend/ETL/extract.php
@@ -0,0 +1,27 @@
+
+<?php
+
+function fetchRadioSRFdata() {
+    $url = "https://il.srgssr.ch/integrationlayer/2.0/srf/songList/radio/byChannel/69e8ac16-4327-4af4-b873-fd5cd6e895a7";
+
+    // Initialisiert eine cURL-Sitzung
+    $ch = curl_init($url);
+
+    // Setzt Optionen
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Führt die cURL-Sitzung aus und erhält den Inhalt
+    $response = curl_exec($ch);
+
+    // Schließt die cURL-Sitzung
+    curl_close($ch);
+
+    // Dekodiert die JSON-Antwort und gibt Daten zurück
+    return json_decode($response, true);
+}
+
+
+echo '<pre>';
+var_dump(fetchRadioSRFdata());
+echo '</pre>';
+?>
